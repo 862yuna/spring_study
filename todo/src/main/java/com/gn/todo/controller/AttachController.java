edf1fce3b4a1256f30efa3ffe81251430416d1f9
862yuna <862yuna@gmail.com>
2025-04-02 11:41:29 +0900

    [cya] CI/CD test 재시도

--- todo/src/main/java/com/gn/todo/controller/AttachController.java
+++ todo/src/main/java/com/gn/todo/controller/AttachController.java
@@ -44,6 +44,7 @@ public class AttachController {
 		
 		System.out.println("CI/ CD test02");
 		System.out.println("test2");
+		System.out.println("yml파일 수정 후 test");
 		
 		Map<String,String> resultMap = new HashMap<String,String>();
 		resultMap.put("res_code", "500");
